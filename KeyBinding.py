b0c7a9c5511f62e56bb24ffdefe0ac09b8d4f23a
zongzi <545670755@qq.com>
2019-03-20 19:34:03 +0800

    添加了代码备注

--- KeyBinding.py
+++ KeyBinding.py
@@ -7,17 +7,33 @@ kb = KeyBindings()
 
 @kb.add('c-q')
 def exit_(event):
+    '''
+    退出快捷键
+    :param event: 事件内容
+    :return: null
+    '''
     display_container.alarm_thread.stop()
     event.app.exit()
 
 
 @kb.add('c-w')
 def stop_(event):
+    '''
+    停止计时快捷键
+    :param event: 事件内容
+    :return:null
+    '''
     display_container.alarm_thread.stop()
 
 
 @kb.add('c-s')
 def start_(event):
+    '''
+    开始快捷键
+    :param event:事件内容
+    :return: null
+    '''
+
     if display_container.alarm_thread.stop_flag:
         display_container.alarm_thread = AlarmThread(buffer)
         display_container.alarm_thread.start()
